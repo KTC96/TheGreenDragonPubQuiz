0b4df4a011ed3c87da8207eb6d597f5d2562414a
KTC96 <kyleclow96@gmail.com>
2023-06-06 20:20:13 +0000

    Add functions to increment score but need to get functional

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -22,12 +22,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function displayQuestionAndAnswers() {
 
+    document.getElementsByClassName("answer-button").value = "";
 
 
-   
-    const questions = [`Who is the steward of Gondor before Aragorn returns to claim the throne?`, `What is the name of the horse given to Aragorn by Rohan's king?`, `Who is the steward of the Ring who succumbs to its power and attempts to take it for himself?`, `What is the name of the Shire's capital, where the hobbits reside?`, `Who is the founder and of Rohan?`,`Who is the wizard known as the Grey Pilgrim?`,`What is the name of the mountain where the One Ring is destroyed?`,`Which character famously says, "Not all those who wander are lost"?`,`What is the name of the ancient language spoken by the elves in Middle-earth?`,`What is the language of Mordor, spoken by Sauron and his servants?`,`What is the elvish city that serves as a refuge for the Fellowship in "The Fellowship of the Ring"`,`What is Legolas's surname?`,`What is the name of the river that flows through Gondor and is guarded by the fortress of Osgiliath?`,`Who is the leader of the Nazgûl?`,`What is the name of the fortress and city ruled by the steward Denethor in Gondor?`,`What is the name of the elf who forges the rings of power in Eregion?`,`What is the name of the horse ridden by Gandalf?`,`What is the name of the evil spider-like creature that captures Frodo?`,` What is the name of the fortress that serves as the dwelling place of Sauron in Mordor?`,`What is the name of the kingdom of the dwarves that Thorin Oakenshield seeks to reclaim in "The Hobbit"?`];
 
-    
+
+    const questions = [`Who is the steward of Gondor before Aragorn returns to claim the throne?`, `What is the name of the horse given to Aragorn by Rohan's king?`, `Who is the steward of the Ring who succumbs to its power and attempts to take it for himself?`, `What is the name of the Shire's capital, where the hobbits reside?`, `Who is the founder and of Rohan?`, `Who is the wizard known as the Grey Pilgrim?`, `What is the name of the mountain where the One Ring is destroyed?`, `Which character famously says, "Not all those who wander are lost"?`, `What is the name of the ancient language spoken by the elves in Middle-earth?`, `What is the language of Mordor, spoken by Sauron and his servants?`, `What is the elvish city that serves as a refuge for the Fellowship in "The Fellowship of the Ring"`, `What is Legolas's surname?`, `What is the name of the river that flows through Gondor and is guarded by the fortress of Osgiliath?`, `Who is the leader of the Nazgûl?`, `What is the name of the fortress and city ruled by the steward Denethor in Gondor?`, `What is the name of the elf who forges the rings of power in Eregion?`, `What is the name of the horse ridden by Gandalf?`, `What is the name of the evil spider-like creature that captures Frodo?`, ` What is the name of the fortress that serves as the dwelling place of Sauron in Mordor?`, `What is the name of the kingdom of the dwarves that Thorin Oakenshield seeks to reclaim in "The Hobbit"?`];
+
+
     const questionAnswers = [
         ['Denethor', 'Theoden', 'Boromir', 'Sauron'],
         ['Brego', 'Shadowfax', 'Hasufel', 'Asfaloth'],
@@ -50,6 +52,8 @@ function displayQuestionAndAnswers() {
         ['Barad-dur', 'Minis Morgul', 'Mordor', 'Isengard'],
         ['Erebor', 'Gondor', 'The Shire', 'Rhûn']
     ];
+
+    const correctAnswers = []
 
 
     const usedQuestions = [];
@@ -92,6 +96,9 @@ function displayQuestionAndAnswers() {
             const button = document.createElement('button');
             button.textContent = answer;
             button.className = 'answer-button';
+            button.addEventListener('click', () => {
+                displayQuestionAndAnswers();
+            });
             buttonContainer.appendChild(button);
         });
     }
@@ -113,3 +120,6 @@ function displayQuestionAndAnswers() {
 }
 
 displayQuestionAndAnswers();
+
+
+
