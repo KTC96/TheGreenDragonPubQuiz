335b143a24929a05217df7f6bfab7c6ab7326bed
KTC96 <kyleclow96@gmail.com>
2023-06-07 13:59:43 +0000

    change array type for questions to simplify. Removed random question order and answer order as it made it harder to implement other elements. Solved correct and incorrect answer tally

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,125 +1,187 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let buttons = document.getElementsByTagName("button");
-    for (let button of buttons) {
-        button.addEventListener("click", function () {
-            if (this.getAttribute("data-type") === "submit") {
-                checkAnswer();
-            } else {
-                let gameType = this.getAttribute("data-type");
-                runGame(gameType);
-            }
-        });
+const quizData = [{
+        question: 'Who is the steward of Gondor before Aragorn returns to claim the throne?',
+        answers: ['Denethor', 'Theoden', 'Boromir', 'Sauron'],
+        correctAnswer: 0
+    },
+    {
+        question: "What is the name of the horse given to Aragorn by Rohan's king?",
+        answers: ['Shadowfax', 'Brego', 'Asfaloth', 'Hasufel'],
+        correctAnswer: 1
+    },
+    {
+        question: 'Who is the steward of the Ring who succumbs to its power and attempts to take it for himself?',
+        answers: ['Boromir', 'Faramir', 'Gimli', 'Frodo'],
+        correctAnswer: 0
+    },
+    {
+        question: `What is the name of the Shire's capital, where the hobbits reside?`,
+        answers: ['Longbottom', 'Hardbottle', 'Bree', 'Hobbiton'],
+        correctAnswer: 3
+    },
+    {
+        question: 'Who is the steward of Gondor before Aragorn returns to claim the throne?',
+        answers: ['Boromir', 'Theoden', 'Denethor', 'Sauron'],
+        correctAnswer: 2
+    },
+    {
+        question: "What is the name of the horse given to Aragorn by Rohan's king?",
+        answers: ['Shadowfax', 'Brego', 'Hasufel', 'Asfaloth'],
+        correctAnswer: 1
+    },
+    {
+        question: 'Who is the steward of the Ring who succumbs to its power and attempts to take it for himself?',
+        answers: ['Faramir', 'Frodo', 'Gimli', 'Boromir'],
+        correctAnswer: 3
+    },
+    {
+        question: 'Who is the founder and of Rohan?',
+        answers: ['Theoden', 'King Eorl', 'Halbrand', 'Aragorn'],
+        correctAnswer: 1
+    },
+    {
+        question: 'Who is the wizard known as the Grey Pilgrim?',
+        answers: ['Gandalf', 'Radagast', 'Saruman', 'Alatar'],
+        correctAnswer: 0
+    },
+    {
+        question: 'What is the name of the mountain where the One Ring is destroyed?',
+        answers: ['Erebor', 'Mount Doom', 'Mount Gundabad', 'Thangorodrim'],
+        correctAnswer: 1
+    },
+    {
+        question: 'Which character famously says, "Not all those who wander are lost"?',
+        answers: ['Gandalf', 'Legolas', 'Aragorn', 'Sam'],
+        correctAnswer: 2
+    },
+    {
+        question: 'What is the name of the ancient language spoken by the elves in Middle-earth?',
+        answers: ['Quenya', 'Eltharin', 'Ellylon', 'Sindarin'],
+        correctAnswer: 3
+    },
+    {
+        question: 'What is the language of Mordor, spoken by Sauron and his servants?',
+        answers: ['Black Speech', 'Black Tongue', 'Dark Speech', 'Dark Tongue'],
+        correctAnswer: 0
+    },
+    {
+        question: 'What is the elvish city that serves as a refuge for the Fellowship in "The Fellowship of the Ring"?',
+        answers: ['The Woodland Realm', 'Rivendell', 'Gondolin', 'Grey Havens'],
+        correctAnswer: 1
+    },
+    {
+        question: `What is Legolas's surname?`,
+        answers: ['Netyoive', 'Greenleaf', 'Floshem', 'Mirkwood'],
+        correctAnswer: 1
+
+    },
+    {
+        question: 'What is the name of the river that flows through Gondor and is guarded by the fortress of Osgiliath?',
+        answers: ['Bruinen', 'Glanduin', 'Carnen', 'Anduin'],
+        correctAnswer: 3
+    },
+    {
+        question: 'Who is the leader of the Nazgûl?',
+        answers: ['Sauron', 'Saruman', 'The Mouth Of Sauron', 'The Witch-King of Angmar'],
+        correctAnswer: 3
+    },
+    {
+        question: 'What is the name of the fortress and city ruled by the steward Denethor in Gondor?',
+        answers: ['Osgiliath', 'Minas Tirith', 'Minas Morgul', 'Umbar'],
+        correctAnswer: 1
+    },
+    {
+        question: 'What is the name of the elf who forges the rings of power in Eregion?',
+        answers: ['Celebrimbor', 'Annatar', 'Elrond', 'Arondir'],
+        correctAnswer: 0
+
+    },
+    {
+        question: 'What is the name of the horse ridden by Gandalf?',
+        answers: ['Asfaloth', 'Arod', 'Shadowfax', 'Snowmane'],
+        correctAnswer: 2
+    },
+    {
+        question: 'What is the name of the evil spider-like creature that captures Frodo?',
+        answers: ['Ungoliant', 'Shelob', 'Attercop', 'Cingwin'],
+        correctAnswer: 1
+    },
+    {
+        question: 'What is the name of the fortress that serves as the dwelling place of Sauron in Mordor?',
+        answers: ['Minas Morgul', 'Mordor', 'Isengard', 'Barad-dur'],
+        correctAnswer: 3
+    },
+    {
+        question: 'What is the name of the kingdom of the dwarves that Thorin Oakenshield seeks to reclaim in "The Hobbit"?',
+        answers: ['Erebor', 'Gondor', 'The Shire', 'Rhûn', ],
+        correctAnswer: 0
+
     }
+];
 
+const questionArea = document.getElementById('question');
+const answerButtons = document.querySelectorAll('.answer-button');
+const incorrectCountArea = document.getElementById('incorrect-count');
+const scoreElement = document.getElementById('score');
+const highScoreElement = document.getElementById('high-score');
 
+let currentQuestion = 0;
+let score = 0;
+let incorrect = 0;
+let highScore = 0;
 
-
-    runGame();
-
-
-
-});
-
-function displayQuestionAndAnswers() {
-
-    document.getElementsByClassName("answer-button").value = "";
-
-
-
-
-    const questions = [`Who is the steward of Gondor before Aragorn returns to claim the throne?`, `What is the name of the horse given to Aragorn by Rohan's king?`, `Who is the steward of the Ring who succumbs to its power and attempts to take it for himself?`, `What is the name of the Shire's capital, where the hobbits reside?`, `Who is the founder and of Rohan?`, `Who is the wizard known as the Grey Pilgrim?`, `What is the name of the mountain where the One Ring is destroyed?`, `Which character famously says, "Not all those who wander are lost"?`, `What is the name of the ancient language spoken by the elves in Middle-earth?`, `What is the language of Mordor, spoken by Sauron and his servants?`, `What is the elvish city that serves as a refuge for the Fellowship in "The Fellowship of the Ring"`, `What is Legolas's surname?`, `What is the name of the river that flows through Gondor and is guarded by the fortress of Osgiliath?`, `Who is the leader of the Nazgûl?`, `What is the name of the fortress and city ruled by the steward Denethor in Gondor?`, `What is the name of the elf who forges the rings of power in Eregion?`, `What is the name of the horse ridden by Gandalf?`, `What is the name of the evil spider-like creature that captures Frodo?`, ` What is the name of the fortress that serves as the dwelling place of Sauron in Mordor?`, `What is the name of the kingdom of the dwarves that Thorin Oakenshield seeks to reclaim in "The Hobbit"?`];
-
-
-    const questionAnswers = [
-        ['Denethor', 'Theoden', 'Boromir', 'Sauron'],
-        ['Brego', 'Shadowfax', 'Hasufel', 'Asfaloth'],
-        ['Boromir', 'Faramir', 'Gimli', 'Frodo'],
-        ['Hobbiton', 'Bree', 'Longbottom', 'Hardbottle'],
-        ['King Eorl', 'Theoden', 'Halbrand', 'Aragorn'],
-        ['Gandalf', 'Radagast', 'Saruman', 'Alatar'],
-        ['Mount Doom', 'Erebor', 'Mount Gundabad', 'Thangorodrim'],
-        ['Aragorn', 'Gandalf', 'Legolas', 'Sam'],
-        ['Sindarin', 'Quenya', 'Ellylon', 'Eltharin'],
-        ['Black Speech', 'Black Tongue', 'Dark Speech', 'Dark Tongue'],
-        ['Rivendell', 'The Woodland Realm', 'Gondolin', 'Grey Havens'],
-        ['Greenleaf', 'Netyoive', 'Floshem', 'Mirkwood'],
-        ['Anduin', 'Bruinen', 'Glanduin', 'Carnen'],
-        ['The Witch-King of Angmar', 'Sauron', 'The Mouth Of Sauron', 'Saruman'],
-        ['Minas Tirith', 'Osgiliath', 'Minas Morgul', 'Umbar'],
-        ['Celibrimbor', 'Annatar', 'Elrond', 'Arondir'],
-        ['Shadowfax', 'Asfaloth', 'Arod', 'Snowmane'],
-        ['Shelob', 'Ungoliant', 'Attercop', 'Cingwin'],
-        ['Barad-dur', 'Minis Morgul', 'Mordor', 'Isengard'],
-        ['Erebor', 'Gondor', 'The Shire', 'Rhûn']
-    ];
-
-    const correctAnswers = []
-
-
-    const usedQuestions = [];
-
-    function displayQuestionAndAnswers() {
-
-        if (usedQuestions.length === questions.length) {
-            const questionArea = document.getElementById('question-area');
-            questionArea.textContent = 'You have completed the quiz';
-            return;
-        }
-
-        let randomIndex;
-        do {
-            randomIndex = Math.floor(Math.random() * questions.length);
-        } while (usedQuestions.includes(randomIndex));
-
-
-        usedQuestions.push(randomIndex);
-
-        const questionArea = document.getElementById('question-area');
-
-
-        questionArea.textContent = questions[randomIndex];
-
-
-        const buttonContainer = document.getElementById('button-container');
-
-
-        buttonContainer.innerHTML = '';
-
-
-        const answers = questionAnswers[randomIndex];
-
-
-        const shuffledAnswers = shuffleArray([...answers]);
-
-
-        shuffledAnswers.forEach(answer => {
-            const button = document.createElement('button');
-            button.textContent = answer;
-            button.className = 'answer-button';
-            button.addEventListener('click', () => {
-                displayQuestionAndAnswers();
-            });
-            buttonContainer.appendChild(button);
-        });
-    }
-
-
-    displayQuestionAndAnswers();
-
-
-    function shuffleArray(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
-    }
-
-
-
+function initializeQuiz() {
+    showQuestion();
+    answerButtons.forEach((button) => {
+        button.addEventListener('click', (event) => handleAnswer(event));
+    });
 }
 
-displayQuestionAndAnswers();
+function showQuestion() {
+    const question = quizData[currentQuestion];
+    questionArea.textContent = question.question;
 
+    for (let i = 0; i < answerButtons.length; i++) {
+        answerButtons[i].textContent = question.answers[i];
+    }
+}
 
+function handleAnswer(event) {
+    const selectedButton = event.target;
+    const question = quizData[currentQuestion];
+    const selectedAnswer = Array.from(answerButtons).indexOf(selectedButton);
 
+    if (selectedAnswer === question.correctAnswer) {
+        score++;
+        updateScore();
+    } else {
+        incorrect++;
+        updateIncorrectCount();
+    }
+
+    currentQuestion++;
+
+    if (currentQuestion < quizData.length) {
+        showQuestion();
+    } else {
+        showFinalScore();
+    }
+}
+
+function updateScore() {
+    scoreElement.textContent = score;
+}
+
+function updateIncorrectCount() {
+    incorrectCountArea.textContent = incorrect;
+}
+
+function showFinalScore() {
+    if (score > highScore) {
+        highScore = score;
+        highScoreElement.textContent = highScore;
+    }
+
+    questionArea.textContent = `Quiz Finished!\nYour Score: ${score}/${quizData.length}`;
+}
+
+initializeQuiz();
